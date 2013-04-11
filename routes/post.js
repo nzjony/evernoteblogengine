72ddde96237d74fa945bd3b17bec50bf89606c89
Jonathan S <jonotkd@gmail.com>
2013-04-11 19:56:32 +0200

    Adding page to render a single post by itself (and includes the comments)

---
+++ routes/post.js
@@ -0,0 +1,15 @@
+exports.post = function(req, res) {
+	var post = blogContent.filter(function (post) {
+		return post.htmlsafetitle == req.params.postname;
+	});
+	if(post.length == 0) {
+		res.end();
+	} else {
+		//post is an array with one object, so pull it out, this may break
+		//if I ever have two posts with the same name. Could potentially add
+		//the date to it... think about it for now.
+		post = post[0];
+		//Title could be put in a config file... todo
+		res.render('post', {title: 'Jonathan Stichbury\'s Adventures', item: post});
+	}	
+};
